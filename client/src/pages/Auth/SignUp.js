51c69b2d76b8cfdf0f2f32e1f3fa2dcf3768b450
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-02-18 19:48:40 +0600

    signin and signup page design

---
+++ client/src/pages/Auth/SignUp.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Layout from "../../components/Layout/Layout"
+import Microscope from "../../img/img3c.png"
+import "./auth.css"
+import { NavLink } from "react-router-dom"
+
+
+const SignUp = () => {
+    return (
+        <Layout>
+            <div className="signup_login">
+                <div className="signup">
+                    <img src={Microscope} alt="microscope" className="img3" />
+                </div>
+
+                <form action="" className="userform">
+                    <h2>Sign Up</h2>
+                    <div className="input-group">
+                        <input type="text" required />
+                        <label for="">Full Name</label>
+                    </div>
+                    <div className="input-group">
+                        <input type="email" required />
+                        <label for="">Email</label>
+                    </div>
+                    <div className="input-group">
+                        <input type="password" required />
+                        <label for="">Password</label>
+                    </div>
+                    <div className="remember">
+                        <label
+                        ><input type="checkbox" /> I agree to the terms & conditions</label
+                        >
+                    </div>
+                    <button type="submit" className="button">Sign Up</button>
+                    <div className="signUp-link">
+                        <p>
+                            Already have an account?
+                            <NavLink to="/signin" className="signInBtn-link">Sign In</NavLink>
+                        </p>
+                    </div>
+                </form>
+            </div>
+        </Layout>
+    )
+}
+
+export default SignUp
