cde537a6629174937aa9056d512b00ccdb353c8f
Redowanur <rahmanlabib74@gmail.com>
2024-02-18 13:42:41 +0600

    fixes header issue

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,20 +1,21 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import { Menu } from '@material-ui/icons'
 
 const Header = () => {
   return(
-    <header className="w-screen h-14 flex justify-center items-center shadow-md" id="header">
+    <header className="w-screen h-20 flex justify-center items-center shadow-md" id="header">
         <nav className="w-full flex justify-between items-center px-9 lg:w-large">
-            <a href="#" className="nav__logo">
+            <NavLink to='/' className="nav__logo">
             <i class="fa-solid fa-leaf text-sgreen-100"></i><span className='ml-2 uppercase font-extrabold text-sm'> stomascope</span>
-            </a>
+            </NavLink>
             <div className='flex gap-10'>
                 <ul className='md:flex gap-10 hidden'>
-                    <li><a className='hover:underline hover:decoration-sgreen-100 hover:decoration-2 hover:underline-offset-4 hover:text-sgreen-100'>Home</a></li>
-                    <li><a className='hover:underline hover:decoration-sgreen-100 hover:decoration-2 hover:underline-offset-4 hover:text-sgreen-100'>About</a></li>
-                    <li><a className='hover:underline hover:decoration-sgreen-100 hover:decoration-2 hover:underline-offset-4 hover:text-sgreen-100'>Procedure</a></li>
-                    <li><a className='hover:underline hover:decoration-sgreen-100 hover:decoration-2 hover:underline-offset-4 hover:text-sgreen-100'>FAQs</a></li>
-                    <li><a className='hover:underline hover:decoration-sgreen-100 hover:decoration-2 hover:underline-offset-4 hover:text-sgreen-100'>Contact Us</a></li>
+                    <li><NavLink to='/' className='hover:cursor-pointer hover:underline hover:decoration-sgreen-100 hover:decoration-2 hover:underline-offset-4 hover:text-sgreen-100'>Home</NavLink></li>
+                    <li><NavLink className='hover:cursor-pointer hover:underline hover:decoration-sgreen-100 hover:decoration-2 hover:underline-offset-4 hover:text-sgreen-100'>Detect</NavLink></li>
+                    <li><NavLink to='/' className='hover:cursor-pointer hover:underline hover:decoration-sgreen-100 hover:decoration-2 hover:underline-offset-4 hover:text-sgreen-100'>History</NavLink></li>
+                    <li><NavLink className='hover:cursor-pointer hover:underline hover:decoration-sgreen-100 hover:decoration-2 hover:underline-offset-4 hover:text-sgreen-100'>FAQs</NavLink></li>
+                    <li><NavLink className='hover:cursor-pointer hover:underline hover:decoration-sgreen-100 hover:decoration-2 hover:underline-offset-4 hover:text-sgreen-100'>Contact Us</NavLink></li>
                 </ul>
                 <div className='md:hidden'><Menu/></div>
             </div>
--- client/src/pages/History/History.js
+++ client/src/pages/History/History.js
@@ -1,22 +1,10 @@
 import React from 'react'
 import Layout from "../../components/Layout/Layout"
-import "./history.css"
-import datas from "./data.js"
-import Item from '../../components/Items/Item'
 
 const History = () => {
     return (
         <Layout>
-            <div className='history'>
-                <h1>Your History</h1>
-                <div className="history-items">
-                    {
-                        datas.map((data, idx) => {
-                            return <Item key={idx} id={data.id} image={data.image} title={data.title} count={data.count} />
-                        })
-                    }
-                </div>
-            </div>
+
         </Layout>
     )
 }
