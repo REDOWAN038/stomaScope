58ac9994df1f85c5a98390e2c47a6ece3a6aad82
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-02-18 14:33:28 +0600

    signin, signup button

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,28 +1,39 @@
 import React from 'react'
-import { NavLink } from "react-router-dom"
+import { NavLink, useLocation } from "react-router-dom"
 import { Menu } from '@material-ui/icons'
 import "../../css/tailwind.css"
 
 const Header = () => {
-  return (
-    <header className="w-screen h-14 flex justify-center items-center shadow-md" id="header">
-      <nav className="w-full flex justify-between items-center px-9 lg:w-large">
-        <NavLink to="/" className="nav__logo header-navlink">
-          <i class="fa-solid fa-leaf text-sgreen-100"></i><span className='ml-2 uppercase font-extrabold text-sm'> stomascope</span>
-        </NavLink>
-        <div className='flex gap-10'>
-          <ul className='md:flex gap-10 hidden'>
-            <li><NavLink to="/" className='hover:underline hover:decoration-sgreen-100 hover:decoration-2 hover:underline-offset-4 hover:text-sgreen-100 header-navlink'>Home</NavLink></li>
-            <li><NavLink className='header-navlink hover:underline hover:decoration-sgreen-100 hover:decoration-2 hover:underline-offset-4 hover:text-sgreen-100'>Detect</NavLink></li>
-            <li><NavLink to="/history" className='header-navlink hover:underline hover:decoration-sgreen-100 hover:decoration-2 hover:underline-offset-4 hover:text-sgreen-100'>History</NavLink></li>
-            <li><NavLink className='header-navlink hover:underline hover:decoration-sgreen-100 hover:decoration-2 hover:underline-offset-4 hover:text-sgreen-100'>FAQs</NavLink></li>
-            <li><NavLink className='header-navlink hover:underline hover:decoration-sgreen-100 hover:decoration-2 hover:underline-offset-4 hover:text-sgreen-100'>Contact Us</NavLink></li>
-          </ul>
-          <div className='md:hidden'><Menu /></div>
-        </div>
-      </nav>
-    </header>
-  )
+    const location = useLocation()
+    const isActive = (path) => {
+        return location.pathname === path ? "border-b-2 border-sgreen-100 text-sgreen-100" : ""
+    }
+    return (
+        <header className="w-screen h-14 flex justify-center items-center shadow-md" id="header">
+            <nav className="w-full flex justify-between items-center px-9 lg:w-large">
+                <NavLink to="/" className="nav__logo header-navlink">
+                    <i class="fa-solid fa-leaf text-sgreen-100"></i><span className='ml-2 uppercase font-extrabold text-sm'> stomascope</span>
+                </NavLink>
+                <div className='flex gap-10'>
+                    <ul className='md:flex gap-10 hidden'>
+                        <li><NavLink to="/" className={`header-navlink hover:decoration-sgreen-100 hover:decoration-2  hover:text-sgreen-100 ${isActive('/')}`}>Home</NavLink></li>
+                        <li><NavLink className={`header-navlink hover:decoration-sgreen-100 hover:decoration-2  hover:text-sgreen-100 ${isActive('/detect')}`}>Detect</NavLink></li>
+                        <li><NavLink to="/history" className={`header-navlink hover:decoration-sgreen-100 hover:decoration-2  hover:text-sgreen-100 ${isActive('/history')}`}>History</NavLink></li>
+                        <li><NavLink className={`header-navlink hover:decoration-sgreen-100 hover:decoration-2  hover:text-sgreen-100 ${isActive('/faqs')}`}>FAQs</NavLink></li>
+                        <li><NavLink className='header-navlink  hover:decoration-sgreen-100 hover:decoration-2  hover:text-sgreen-100'>Contact Us</NavLink></li>
+                    </ul>
+                    <div className='md:hidden'><Menu /></div>
+                </div>
+                <div className='flex gap-10'>
+                    <ul className='md:flex gap-3 hidden'>
+                        <li><button className="bg-white border-2 border-black text-sm text-black px-3 py-1 rounded-full">Sign In</button></li>
+                        <li><button className="bg-white border-2 border-black text-sm text-black px-3 py-1 rounded-full">Sign Up</button></li>
+                    </ul>
+                    <div className='md:hidden'><Menu /></div>
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Header
