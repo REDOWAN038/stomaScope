82e320d710379c20f9de12cb97bb541feff1f051
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-02-18 18:32:08 +0600

    some silly modifications

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -4,13 +4,16 @@ import "../css/tailwind.css"
 
 const HeroSection = () => {
     return (
-        <section className="flex flex-col md:flex-row-reverse justify-center md:justify-between items-center my-16 lg:w-large lg:mx-auto md:mt-20 lg:mt-32" id="home">
+        <section className="flex flex-col md:flex-row-reverse justify-center md:justify-between items-center my-2 lg:w-large lg:mx-auto md:mt-20 lg:mt-32" id="home">
             <img src={Poster} className="w-52 md:w-64 md:mr-8 lg:w-1/3" />
             <div className="w-fit md:w-96 mx-6 my-6 md:ml-8 lg:w-1/2">
                 <div className="font-semibold text-3xl lg:text-5xl tracking-wide w-96 lg:w-full">
                     <h1 className="leading-snug">Detect stomata from leaf image and video</h1>
                 </div>
-                <p className="my-6 leading-6 text-sm lg:text-base font-light tracking-wider text-gray-700">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                <p className="my-6 leading-6 text-sm text-wrap lg:text-base font-light tracking-wider text-gray-700">
+                    Our advanced algorithm accurately detects stomata from
+                    microscopic images and videos of leaves. With our cutting edge
+                    technology, you can easily analyze stomata.</p>
                 <button className="bg-sgreen-100 text-white py-4 px-6 rounded-xl">Explore <i class="fa-solid fa-arrow-right"></i></button>
             </div>
         </section>
--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
     }
     return (
         <header className="w-screen h-14 flex justify-center items-center shadow-md" id="header">
-            <nav className="w-full flex justify-between items-center px-9 lg:w-large">
+            <nav className="w-full flex justify-between items-center px-9 lg:w-large text-xs">
                 <NavLink to="/" className="nav__logo header-navlink">
                     <i class="fa-solid fa-leaf text-sgreen-100"></i><span className='ml-2 uppercase font-extrabold text-sm'> stomascope</span>
                 </NavLink>
@@ -22,15 +22,14 @@ const Header = () => {
                         <li><NavLink className={`header-navlink hover:decoration-sgreen-100 hover:decoration-2  hover:text-sgreen-100 ${isActive('/faqs')}`}>FAQs</NavLink></li>
                         <li><NavLink className='header-navlink  hover:decoration-sgreen-100 hover:decoration-2  hover:text-sgreen-100'>Contact Us</NavLink></li>
                     </ul>
-                    <div className='md:hidden'><Menu /></div>
                 </div>
                 <div className='flex gap-10'>
                     <ul className='md:flex gap-3 hidden'>
-                        <li><button className="bg-white border-2 border-black text-sm text-black px-3 py-1 rounded-full">Sign In</button></li>
-                        <li><button className="bg-white border-2 border-black text-sm text-black px-3 py-1 rounded-full">Sign Up</button></li>
+                        <li><button className="bg-sgreen-100 border-2 border-sgreen-100 text-xs text-white px-3 py-1 rounded-full ">Sign In</button></li>
+                        <li><button className="bg-white border-2 border-black text-xs text-black px-3 py-1 rounded-full">Sign Up</button></li>
                     </ul>
-                    <div className='md:hidden'><Menu /></div>
                 </div>
+                <div className='md:hidden'><Menu /></div>
             </nav>
         </header>
     )
