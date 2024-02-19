d1e161e4f3cbb757889f37846cce6af8f339f4ca
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-02-19 16:57:51 +0600

    fetching user upload history

--- client/src/components/Items/Item.js
+++ client/src/components/Items/Item.js
@@ -1,12 +1,12 @@
 import React from 'react'
 // import "./item.css"
 
-const Item = ({image, title, count}) => {
+const Item = ({ image, name, count }) => {
     return (
         <div className='max-w-96 shadow-md rounded-xl'>
-            <img src={image} className='rounded-t-xl w-[350px] h-44 object-cover'/>
+            <img src={image} className='rounded-t-xl w-[350px] h-44 object-cover' />
             <div className='m-3'>
-                <div><h1 className='font-semibold tracking-wide'>{title}</h1></div>
+                <div><h1 className='font-semibold tracking-wide'>{name}</h1></div>
                 <div><p className='text-sm font-light text-gray-800 my-2'>Count: {count}</p> </div>
             </div>
         </div>
--- client/src/pages/History/History.js
+++ client/src/pages/History/History.js
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from "../../components/Layout/Layout"
 import datas from "./data.js"
 import Item from '../../components/Items/Item'
+import axios from "axios"
 
 const History = () => {
+    const [data, setData] = useState([])
+
+    const getUserHistory = async () => {
+        try {
+            const res = await axios.get(`${process.env.REACT_APP_API}/api/v1/users/upload-history`, { withCredentials: true }
+            )
+
+
+            if (res?.data?.success) {
+                setData(res?.data?.payload?.history)
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getUserHistory()
+    }, [])
+
     return (
         <Layout>
             <div className='flex flex-col justify-center items-center'>
                 <h1 className='font-semibold text-2xl tracking-wide my-6'>Your History</h1>
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:w-large'>
                     {
-                        datas.map((val, index) => (
+                        data.map((val, index) => (
                             <Item
                                 key={index}
                                 {...val}
