d2948bb84980b37d4130e385bfefbd74e4f40dda
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-02-20 20:38:41 +0600

    file download

--- client/src/components/Items/Item.js
+++ client/src/components/Items/Item.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import { MoreVert } from '@material-ui/icons';
-import "./item.css"
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { saveAs } from "file-saver"
 
 const Item = ({ item, onImageClick, onDelete }) => {
     const [showDropdown, setShowDropdown] = useState(false)
+
+    const handleDownload = () => {
+        saveAs(item.image, item.name)
+    }
 
     const handleImageClick = () => {
         onImageClick(item)
@@ -38,7 +40,7 @@ const Item = ({ item, onImageClick, onDelete }) => {
                                 <div className='bg-white w-28 shadow-lg absolute right-0'>
                                     <ul>
                                         <li className='p-1 text-sm cursor-pointer rounded hover:bg-blue-100' onClick={handleDelete}>Delete</li>
-                                        <li className='p-1 text-sm cursor-pointer rounded hover:bg-blue-100'>Download</li>
+                                        <li className='p-1 text-sm cursor-pointer rounded hover:bg-blue-100' onClick={handleDownload}>Download</li>
                                     </ul>
                                 </div>
                             )
--- client/src/pages/Detect/Detect.js
+++ client/src/pages/Detect/Detect.js
@@ -3,15 +3,26 @@ import Layout from "../../components/Layout/Layout"
 import Form from 'react-bootstrap/Form';
 import axios from "axios"
 import "./detect.css"
+import SyncLoader from "react-spinners/SyncLoader"
+import { saveAs } from "file-saver"
 
 const Detect = () => {
     const [file, setFile] = useState("")
     const [image, setImage] = useState("")
+    const [name, setName] = useState("")
     const [count, setCount] = useState(0)
+    const [loading, setLoading] = useState(false)
+
+    const handleDownload = () => {
+        saveAs(image, name)
+    }
 
     const uploadImage = async (e) => {
         // e.preventDefault()
         try {
+            setLoading(true)
+            setImage("")
+            setCount(0)
             const name = file.name.split(".")[0]
             const formData = new FormData()
             formData.append("name", name)
@@ -22,12 +33,14 @@ const Detect = () => {
             )
 
             if (res?.data?.success) {
+                setLoading(false)
                 setImage(res?.data?.payload?.file?.image)
                 setCount(res?.data?.payload?.file?.count)
-                // navigate("/history")
+                setName(res?.data?.payload?.file?.name)
             }
         } catch (error) {
-
+            setLoading(false)
+            console.log(error);
         }
     }
 
@@ -42,13 +55,22 @@ const Detect = () => {
                             </Form.Group>
                         </Form>
                     </li>
-                    <li><button className="bg-sgreen-100 border-2 border-sgreen-100 text-xs text-white px-3 py-1 rounded-full mt-1" onClick={() => uploadImage()}>Submit</button></li>
+                    <li><button className="bg-sgreen-100 border-2 border-sgreen-100 text-xs text-white px-3 py-1 rounded-full mt-1" onClick={() => uploadImage()}>Upload</button></li>
                 </ul>
+                <div className='flex justify-center	items-center'>
+                    <SyncLoader
+                        loading={loading}
+                        color='#3E6553'
+                    />
+                </div>
                 {
                     image && (
                         <div className='preview'>
                             <h1 className='height'>Stomata Count : {count}</h1>
                             <img src={image} alt="" className='image' />
+                            {/* <div className='flex justify-center	items-center mt-3'> */}
+                            <button className="bg-sgreen-100 border-2 border-sgreen-100 text-xs text-white px-3 py-1 rounded-full mt-2" onClick={() => handleDownload()}>Download</button>
+                            {/* </div> */}
                         </div>
                     )
                 }
