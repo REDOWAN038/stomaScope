27c30cc33d3a019e0d1aaedef4beb5568fcf61db
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-03-21 19:11:00 +0600

    cors issues 3

--- server/src/app.js
+++ server/src/app.js
@@ -30,7 +30,8 @@ const { clientURL } = require("./secret")
 // });
 app.use(cors({
     origin: clientURL,
-    methods: ["POST", "GET", "PUT", "DELETE"],
+    methods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }))
 app.use(cookieParser())
