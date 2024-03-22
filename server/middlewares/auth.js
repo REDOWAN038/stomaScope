d74442617ab5e51220b2e0e142b06fe060d082a4
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-03-22 15:42:11 +0600

    cookies issue 3

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -5,6 +5,7 @@ const { jwtAccessKey } = require("../src/secret")
 const isLoggedIn = async (req, res, next) => {
     try {
         const token = req.cookies.accessToken
+        console.log("cookies : ", req.cookies);
         if (!token) {
             throw createError(401, "user is not logged in")
         }
