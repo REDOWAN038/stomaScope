be169ff14074053253c54a46d4fa6d3e3b6adff5
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-03-22 11:48:06 +0600

    render issues 3

--- server/src/app.js
+++ server/src/app.js
@@ -21,8 +21,7 @@ const { clientURL } = require("./secret")
 
 // middlewares
 app.use((req, res, next) => {
-    const origin = req.headers.origin;
-    res.setHeader('Access-Control-Allow-Origin', origin); // Allow requests from any origin
+    res.setHeader('Access-Control-Allow-Origin', clientURL);
     res.setHeader('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE");
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials (cookies)
